e47d4f2abf1b6d6d9c2825052cc8f6d4b3f8fb5c
José Antônio <joseeantonioo2000@gmail.com>
2024-09-04 13:33:23 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import GlobalStyles from './components/global.js'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GlobalStyles />
+    <App />
+  </StrictMode>,
+)
